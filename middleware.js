6ee0bf739ddedbfbf2367e9747207eab196029a9
ARYAN BARSAIYAN <aryanbarsaiyan@ARYANs-MacBook-Air.local>
2024-10-04 17:32:15 +0530

    .

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 function validateApiKey(req, res, next) {
+    console.log('Validating API Key');
     const apiKey = req.headers['api-key'];
     if (apiKey !== process.env.API_KEY) {
         return res.status(401).json({ message: 'Unauthorized: Invalid API Key' });
