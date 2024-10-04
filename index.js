8f0d20b057acf35d32b755ceafd696e7c6a359b0
ARYAN BARSAIYAN <aryanbarsaiyan@ARYANs-MacBook-Air.local>
2024-10-04 17:54:27 +0530

    done

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/health', validateApiKey, (req, res) => {
 });
 
 // Routes
-app.use('/warmup', warmupRoutes);
+app.use('/warmup', validateApiKey, warmupRoutes);
 
 // Root endpoint
 app.get('/', (req, res) => {
